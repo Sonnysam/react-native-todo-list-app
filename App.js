0078e477e19346f3b3462c292439310c382de32b
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2022-05-31 02:48:51 +0000

    code refactored and header component added

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
-
+import Header from "./components/header";
 
 export default function App() {
   const [todos, setTodos] = useState([
     { text: "Learn React Native", key: "1" },
-    { text: "Learn Calculus", key: "2" },
+    { text: "Build a Simple Project", key: "2" },
     { text: "Rest", key: "3" },
   ]);
 
   return (
     <View style={styles.container}>
-      <View>
-        <FlatList
-          data={todos}
-          renderItem={({ item }) => (
-            <Text style={styles.item}>{item.text}</Text>
-          )}
-        />
+      <Header />
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text style={styles.item}>{item.text}</Text>
+            )}
+          />
+        </View>
       </View>
     </View>
   );
 }
 
+//STYLES SECTION
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
